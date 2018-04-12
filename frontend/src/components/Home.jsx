f163c5f1426b4c7d5108ff5c8fcad32e8462a7c0
Anthony Wong <anthony.wong182@gmail.com>
2018-04-12 15:00:11 -0700

    form handlers for login/signup

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,10 +6,29 @@ import axios from 'axios'
 const FormItem = Form.Item
 
 class Home extends Component {
+  state = {
+    username: '',
+    password: ''
+  }
 
+  // FORM HANDLERS
+  usernameHandler = (ev) => {
+    this.setState({
+      username: ev.target.value
+    })
+  }
+
+  passwordHandler = (ev) => {
+    this.setState({
+      password: ev.target.value
+    })
+  }
+
+  // SIGN IN
   userLogin = () => {
     axios.post('http://localhost:6060/login', {
-      value: 'test'
+      username: this.state.username,
+      password: this.state.password
     })
     .then(function (response) {
       console.log(response)
@@ -24,23 +43,47 @@ class Home extends Component {
     return (
       <div className="content">
         <Form>
+
           {/* LOG IN FORM */}
           <FormItem>
             {getFieldDecorator('userName', {
-              rules: [{ required: true, message: 'Please input your username!' }],
+              rules: [
+                { 
+                  required: true, 
+                  message: 'Please input your username!' 
+                }
+              ],
             })(
-              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+              <Input 
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="Username" 
+                onChange={this.usernameHandler}/>
             )}
           </FormItem>
           <FormItem>
             {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
+              rules: [
+                { 
+                  required: true, 
+                  message: 'Please input your Password!' 
+                }
+              ],
             })(
-              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+              <Input 
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} 
+                type="password" 
+                placeholder="Password" 
+                onChange={this.passwordHandler}/>
             )}
           </FormItem>
+
           {/* BUTTONS */}
-          <Button onClick={this.userLogin} icon="heart-o" size='large' ghost>LOG IN</Button>
+          <Button 
+            onClick={this.userLogin} 
+            icon="heart-o" 
+            size='large' 
+            ghost>LOG IN
+          </Button>
           <Link to='/profile'>
           <Button icon="user-add" size='large' ghost>SIGN UP</Button>
           </Link>
--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -1,15 +1,83 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import { Form, Input, Button } from 'antd'
+import axios from 'axios'
 const FormItem = Form.Item
 
 class User extends Component {
   state = {
-
+    confirmDirty: false,
+    email: '',
+    username: '',
+    password: ''
   }
 
-  newUser = (ev) => {
-    console.log(ev.target)
+  // THIS NEEDS WORK. 
+  // IT NEEDS TO PREVENT DEFAULT WHEN FORMS ARE NOT FILLED AND SUMBIT IF THEY ARE
+  handleSubmit = (ev) => {
+    ev.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  // FORM HANDLERS
+  emailHandler = (ev) => {
+    this.setState({
+      email: ev.target.value
+    })
+  }
+
+  usernameHandler = (ev) => {
+    this.setState({
+      username: ev.target.value
+    })
+  }
+
+  passwordHandler = (ev) => {
+    this.setState({
+      password: ev.target.value
+    })
+  }
+
+  // SUBMIT TO BACKEND
+  newUser = () => {
+    axios.post('http://localhost:6060/newUser', {
+      email: this.state.email,
+      username: this.state.username,
+      password: this.state.password
+    })
+    .then(function (response) {
+      console.log(response)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+  }
+
+  // PASSWORD VALIDATION
+  compareToFirstPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Two passwords that you enter is inconsistent!');
+    } else {
+      callback();
+    }
+  }
+
+  validateToNextPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(['confirm'], { force: true });
+    }
+    callback();
+  }
+
+  handleConfirmBlur = (e) => {
+    const value = e.target.value;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
   }
 
   render() {
@@ -27,7 +95,7 @@ class User extends Component {
     // }
     return (
       <div className="content">
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <FormItem
             // {...formItemLayout}
             label="E-mail">
@@ -35,14 +103,26 @@ class User extends Component {
               rules: [
                 {
                   type: 'email', 
-                  message: 'This is not valid E-mail',
+                  message: 'This is not valid E-mail'
                 }, 
                 {
                   required: true, 
-                  message: 'Please enter an E-mail!',
+                  message: 'Please enter an E-mail!'
                 }
               ],
-            })(<Input />)}
+            })(<Input onChange={this.emailHandler} />)}
+          </FormItem>
+          <FormItem
+            // {...formItemLayout}
+            label="Username">
+            {getFieldDecorator('username', {
+              rules: [
+                { 
+                  required: true, 
+                  message: 'Please enter a username'
+                }
+              ]
+            })(<Input onChange={this.usernameHandler} />)}
           </FormItem>
           <FormItem
             // {...formItemLayout}
@@ -51,27 +131,28 @@ class User extends Component {
               rules: [
                 {
                   required: true, 
-                  message: 'Please enter a password',
+                  message: 'Please enter a password'
                 }, 
                 {
-                validator: this.validateToNextPassword,
+                  validator: this.validateToNextPassword
                 }
               ]
-            })(<Input type="password" />)}
+            })(<Input type="password" onChange={this.passwordHandler} />)}
           </FormItem>
           <FormItem
             // {...formItemLayout}
-            label="Username">
-            {getFieldDecorator('nickname', {
+            label="Confirm Password">
+            {getFieldDecorator('confirm', {
               rules: [
-                { 
+                {
                   required: true, 
-                  message: 'Please enter a username'
+                  message: 'Please confirm your password'
+                }, 
+                {
+                  validator: this.compareToFirstPassword
                 }
-              ]
-            })(
-              <Input />
-            )}
+              ],
+            })(<Input type="password" onBlur={this.handleConfirmBlur} />)}
           </FormItem>
           <FormItem>
             <Button
